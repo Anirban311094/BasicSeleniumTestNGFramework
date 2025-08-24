f2b0dabb622010e09ea63eb242237e4a6359999b
Anirban311094 <anirbanc1994@gmail.com>
2025-08-24 12:07:36 +0530

    Commiting DriverFactory for headless execution

--- src/test/java/com/example/framework/core/DriverFactory.java
+++ src/test/java/com/example/framework/core/DriverFactory.java
@@ -1,6 +1,5 @@
 package com.example.framework.core;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,22 +18,57 @@ public class DriverFactory {
 
     public static void initDriver(String browser) {
         if (driver.get() == null) {
+            boolean isCI = System.getenv("GITHUB_ACTIONS") != null; // Detect GitHub Actions env
+
             switch (browser.toLowerCase()) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver.set(new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*")));
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--remote-allow-origins=*");
+
+                    if (isCI) {
+                        chromeOptions.addArguments("--headless=new");
+                        chromeOptions.addArguments("--disable-gpu");
+                        chromeOptions.addArguments("--no-sandbox");
+                        chromeOptions.addArguments("--disable-dev-shm-usage");
+                        chromeOptions.addArguments("--window-size=1920,1080");
+                    }
+
+                    driver.set(new ChromeDriver(chromeOptions));
                     break;
+
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver.set(new FirefoxDriver(new FirefoxOptions()));
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                    if (isCI) {
+                        firefoxOptions.addArguments("--headless");
+                        firefoxOptions.addArguments("--width=1920");
+                        firefoxOptions.addArguments("--height=1080");
+                    }
+
+                    driver.set(new FirefoxDriver(firefoxOptions));
                     break;
+
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver.set(new EdgeDriver(new EdgeOptions()));
+                    EdgeOptions edgeOptions = new EdgeOptions();
+
+                    if (isCI) {
+                        edgeOptions.addArguments("--headless=new");
+                        edgeOptions.addArguments("--disable-gpu");
+                        edgeOptions.addArguments("--no-sandbox");
+                        edgeOptions.addArguments("--disable-dev-shm-usage");
+                        edgeOptions.addArguments("--window-size=1920,1080");
+                    }
+
+                    driver.set(new EdgeDriver(edgeOptions));
                     break;
+
                 default:
                     throw new IllegalArgumentException("Browser not supported: " + browser);
             }
+
             getDriver().manage().window().maximize();
         }
     }
